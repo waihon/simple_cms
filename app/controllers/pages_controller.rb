53ad5043f47946f1ef142b6757c5a66496fda65a
Waihon Yew <yewwaihon@gmail.com>
2015-06-20 08:44:49 +0800

    Finish Subject's Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,11 @@ class PagesController < ApplicationController
   layout "admin"
 
   before_action :confirm_logged_in
+  before_action :find_subject
 
   def index
-    @pages = Page.sorted
+    #@pages = Page.where(:subject_id => @subject.id).sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -13,19 +15,21 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new({:name => "Default"})
+    @page = Page.new({:subject_id => @subject.id, :name => "Default"})
     @subjects = Subject.order('position ASC')
-    @page_count = Page.count + 1
+    #@page_count = Page.count + 1
+    @page_count = @subject.pages.count + 1
   end
 
   def create
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = "Page created successfully."
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :subject_id => @subject.id)
     else
       @subjects = Subject.order('position ASC')
-      @page_count = Page.count + 1
+      #@page_count = Page.count + 1
+      @page_count = @subject.pages.count + 1
       render('new')
     end
   end
@@ -33,17 +37,19 @@ class PagesController < ApplicationController
   def edit
     @page = Page.find(params[:id])
     @subjects = Subject.order('position ASC')
-    @page_count = Page.count
+    #@page_count = Page.count
+    @page_count = @subject.pages.count
   end
 
   def update
     @page = Page.find(params[:id])
     if @page.update_attributes(page_params)
       flash[:notice] = "Page updated successfully."
-      redirect_to(:action => 'show', :id => @page.id)
+      redirect_to(:action => 'show', :id => @page.id, :subject_id => @subject.id)
     else
       @subjects = Subject.order('position ASC')
-      @page_count = Page.count
+      #@page_count = Page.count
+      @page_count = @subject.pages.count
       render('edit')
     end
   end
@@ -55,7 +61,7 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "Page destroyed successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :subject_id => @subject.id)
   end
 
 
@@ -63,6 +69,12 @@ class PagesController < ApplicationController
 
     def page_params
       params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+    end
+
+    def find_subject
+      if params[:subject_id]
+        @subject = Subject.find(params[:subject_id])
+      end
     end
 
 end
