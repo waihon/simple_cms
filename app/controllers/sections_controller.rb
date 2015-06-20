ad34caffbc9a483a5f919232c76b4a82c52eb28d
Waihon Yew <yewwaihon@gmail.com>
2015-06-20 16:56:50 +0800

    Finish Page's Sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,9 +3,11 @@ class SectionsController < ApplicationController
   layout "admin"
 
   before_action :confirm_logged_in
+  before_action :find_page
 
   def index
-    @sections = Section.sorted
+    #@sections = Section.sorted
+    @sections = @page.sections.sorted
   end
 
   def show
@@ -13,19 +15,22 @@ class SectionsController < ApplicationController
   end
 
   def new
-    @section = Section.new({:name => "Default"})
-    @pages = Page.order('position ASC')
-    @section_count = Section.count + 1
+    @section = Section.new({:page_id => @page.id, :name => "Default"})
+    #@pages = Page.order('position ASC')
+    @pages = @page.subject.pages.sorted
+    #@section_count = Section.count + 1
+    @section_count = @page.sections.count + 1
   end
 
   def create
     @section = Section.new(section_params)
     if @section.save
       flash[:notice] = "Section created successfully."
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :page_id => @page.id)
     else
       @pages = Page.order('position ASC')
-      @section_count = Section.count + 1
+      #@section_count = Section.count + 1
+      @section_count = @page.sections.count + 1
       render('new')
     end
   end
@@ -33,17 +38,19 @@ class SectionsController < ApplicationController
   def edit
     @section = Section.find(params[:id])
     @pages = Page.order('position ASC')
-    @section_count = Section.count
+    #@section_count = Section.count
+    @section_count = @page.sections.count
   end
 
   def update
     @section = Section.find(params[:id])
     if @section.update_attributes(section_params)
       flash[:notice] = "Section updated successfully."
-      redirect_to(:action => 'show', :id => @section.id)
+      redirect_to(:action => 'show', :id => @section.id, :page_id => @page.id)
     else
       @pages = Page.order('position ASC')
-      @section_count = Section.count
+      #@section_count = Section.count
+      @section_count = @page.sections.count
       render('edit')
     end
   end
@@ -55,14 +62,20 @@ class SectionsController < ApplicationController
   def destroy
     section = Section.find(params[:id]).destroy
     flash[:notice] = "Section destroyed successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :page_id => @page.id)
   end
 
 
   private
 
     def section_params
-      params.require(:section).permit(:page_id, :name, :position, :visible, :content_type, :content)
+      params.require(:section).permit(:page_id, :name, :position, :visible, 
+        :content_type, :content)
     end
 
+    def find_page
+      if params[:page_id]
+        @page = Page.find(params[:page_id])
+      end
+    end
 end
